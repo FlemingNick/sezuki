5ffd61e7d8b23f80a6c3b2e75f5babb3957d509c
fleming <fleming.nickelsen@hotmail.de>
2023-10-17 18:11:13 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          SEZUKI
+          SEZUKI!
         </p>
         <a
           className="App-link"
